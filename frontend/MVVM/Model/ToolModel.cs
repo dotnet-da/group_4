82a6dc89ad514212cbcb9276156d89326d2db8ba
SameplayerDE <asameplayer@gmail.com>
2022-09-01 17:05:26 +0200

    added mvvm

---
+++ frontend/MVVM/Model/ToolModel.cs
@@ -0,0 +1,10 @@
+﻿namespace frontend.MVVM.Model;
+
+public class ToolModel
+{
+    public partial class Player
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
