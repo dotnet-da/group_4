439a876183cd18cd968b5aec7f877fe43f22f545
Patrick Geertz <geertz.patrick@web.de>
2022-09-03 10:40:13 +0200

    commenting ToolModel.cs

--- backend/Models/ToolModel.cs
+++ backend/Models/ToolModel.cs
@@ -9,8 +9,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace backend.Models
 {
+    ///<summary>
+    ///Model (in MVC desgin pattern) for the stat Tool
+    ///This class has subclasses for every table related to the stat tracking tool in the database
+    ///</summary>
     public class ToolModel
     {
+        ///<summary>
+        /// The player of which an entry was made.
+        ///</summary>
         public partial class Player
         {
             public Player()
@@ -27,6 +34,10 @@ namespace backend.Models
             public virtual ICollection<Collection> Collections { get; set; }
         }
 
+        ///<summary>
+        /// A entry is a specific statistical data, related to a player, a entry-type and a entry-collection.
+        /// This structure allows for easy adding and removing statistical data categorys, by just adding a new entry type.
+        ///</summary>
         public partial class Entry
         {
             [JsonPropertyName("collection_id")]
@@ -43,6 +54,11 @@ namespace backend.Models
             public virtual Collection CollectionNavigation { get; set; }
         }
 
+        ///<summary>
+        /// A Type is mandatory for an entry, because is specifies what data an entry is recording. 
+        /// In a more traditional database structure this would be the column of a table which stores collections of entrys.
+        /// Which this database structure, it allows us to more easily add entry-types or don't use existing ones while the system is already up and running.
+        ///</summary>
         public partial class Type
         {
             public Type()
@@ -58,6 +74,10 @@ namespace backend.Models
             public virtual ICollection<Entry> Entries { get; set; }
         }
         
+        ///<summary>
+        /// A collection is a set of entries all made by a specific player at a specific moment. 
+        /// Every entry should be of a different EntryType (overlapping would be irrational) 
+        ///</summary>
         public partial class Collection
         {
             public Collection()
