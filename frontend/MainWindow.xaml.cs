e15531de4c10e40e2ba41359955446cfd37c28c1
Patrick Geertz <geertz.patrick@web.de>
2022-09-07 22:11:41 +0200

    multible view window added

---
+++ frontend/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using frontend.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace frontend
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new View1ViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new View1ViewModel();
+        }
+
+        private void btnView2_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new View2ViewModel();
+        }
+    }
+}
