35050029d8cad5cc08164a6e73928dca5bd4e4a2
SameplayerDE <asameplayer@gmail.com>
2022-09-09 14:12:39 +0200

    changed ui

--- frontend/MVVM/ViewModel/PlayerViewModel.cs
+++ frontend/MVVM/ViewModel/PlayerViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using System.Windows.Input;
 using frontend.Core;
+using frontend.MVVM.Model;
 using frontend.MVVM.View;
 
 namespace frontend.MVVM.ViewModel
 {
     internal class PlayerViewModel : ObservableObject
     {
+        public PlayerRepository PlayerRepository;
+        
         public ICommand OpenAddPlayerCommand { get; set; }
 
         public PlayerViewModel()
         {
+            PlayerRepository = new PlayerRepository();
             OpenAddPlayerCommand = new Command(
                 _ =>
                 {
