a5e6ca5b2d22b68d0f89b379d58fde17878a9a9d
Patrick Geertz <geertz.patrick@web.de>
2022-09-09 13:03:18 +0200

    addCollection view

--- frontend/MVVM/View/AddCollectionView.xaml.cs
+++ frontend/MVVM/View/AddCollectionView.xaml.cs
@@ -23,5 +23,10 @@ namespace frontend.MVVM.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
