3530502cbd7f112ad551690bce5e31c15d7d0824
SameplayerDE <asameplayer@gmail.com>
2022-09-09 10:59:49 +0200

    script update_backend.sh

--- backend/update_backend.sh
+++ backend/update_backend.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
 sudo systemctl stop RestAPI
-git pull origin master
+git pull origin main
 sudo cp RestAPI.service /etc/systemd/system/RestAPI.service
 sudo systemctl daemon-reload
 dotnet publish -c Release -o /srv/RestAPI/
